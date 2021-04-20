c0ca02159e836849fc1be3895d078d928cd308ae
gotoplace <75188169+gotoplace@users.noreply.github.com>
2021-04-20 21:11:38 +0900

    BOJ 9205 : 맥주 마시면서 걸어가기 - BFS 응용

--- group_study_2nd/group2/week_04/09_BOJ_9205_v1.cpp
+++ group_study_2nd/group2/week_04/09_BOJ_9205_v1.cpp
@@ -4,7 +4,7 @@ https://www.acmicpc.net/problem/9205 : 맥주 마시면서 걸어가기
 BFS로 풀이. 플로이드 워셜, DFS로도 가능할 것으로 보임.
 
 node 배열, visited 배열 활용이 중요한 문제였음.
-[0] : 집 좌표, [1] ~ [N] : 편의점 좌표, [N+1] : 최종 목적지 공연장 좌표
+node 배열 [1] ~ [N] : 편의점 좌표, [N+1] : 최종 목적지 공연장 좌표
 방문 가능한 조건은 abs(dest_x - cur.x) + abs(dest_y - cur.y) <= 1000
 cur는 que front()임
 dest는 node[i]
@@ -18,7 +18,7 @@ int T, N, SX, SY; // TC개수, 편의점 개수
 using pii = pair<int, int>;
 #define x first
 #define y second
-// [0] : 집, [1] ~ [N] : 편의점, [N + 1] : 공연장
+// [1] ~ [N] : 편의점, [N + 1] : 공연장
 pii node[MAXN + 10];
 int visited[MAXN + 10];
 pii que[MAXN * MAXN * 10];
@@ -56,9 +56,8 @@ void Solve() {
   while (T--) {
     cin >> N >> SX >> SY;
     for (int i = 0; i <= N + 1; i++) {
-      node[0].x = 0; node[0].y = 0; // 초기화
+      node[i].x = 0; node[i].y = 0; // 초기화
     }
-    node[0].x = SX; node[0].y = SY; // 집 좌표 입력
     for (int i = 1; i <= N; i++) cin >> node[i].x >> node[i].y; // 편의점 좌표 입력
     cin >> node[N + 1].x >> node[N + 1].y; // 최종 목적지 공연장 입력
     if (BFS()) cout << "happy" << endl;
