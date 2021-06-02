ad16d24e57b80899ceb50c61d08c043c931eb653
gotoplace <75188169+gotoplace@users.noreply.github.com>
2021-06-02 23:22:55 +0900

    BOJ 15663 : N과 M (9) - 백트래킹, 순열 (최적화)

---
+++ Category/Backtracking/09_BOJ_15663_v4.cpp
@@ -0,0 +1,54 @@
+/*
+https://www.acmicpc.net/problem/15663 : N과 M (9)
+2020KB 16ms
+=> prev_num 체크용 변수 하나 추가하여 최적화 (타 사용자 구현 참고)
+
+N개의 자연수를 입력받고 정렬하는 것 외에 15654 N과 M (5) 순열 문제와 동일한 문제
+단, 입력되는 자연수에 중복된 값이 있으며, 중복된 수열을 제거해야 함
+
+순열, 중복순열, 조합, 중복조합 개념
+https://m.blog.naver.com/alwaysneoi/100155625557
+
+순열, 중복순열, 조합, 중복조합 공식
+https://bb-dochi.tistory.com/47
+https://m.blog.naver.com/honeyeah/220295949101
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define MAXN (8)
+
+int N, M;
+int arr[MAXN + 2], used[MAXN + 2], sol[MAXN + 2];
+
+void InputData() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  cin >> N >> M;
+  for (int i = 0; i < N; i++) cin >> arr[i];
+}
+void PrintNum() {
+  for (int i = 0; i < M; i++) cout << sol[i] << " ";
+  cout << endl;
+}
+void DFS(int cnt) {
+  if (cnt == M) {
+    PrintNum(); return;
+  }
+  int prev_num = -1; // 입력된 자연수의 중복 제거를 위한 변수
+  for (int i = 0; i < N; i++) {
+    if (used[i]) continue;
+    if (prev_num == arr[i]) continue;
+    used[i] = 1;
+    sol[cnt] = arr[i];
+    prev_num = arr[i];
+    DFS(cnt + 1);
+    used[i] = 0;
+  }
+}
+int main() {
+  InputData();
+  sort(arr, arr + N);
+  DFS(0);
+  return 0;
+}
